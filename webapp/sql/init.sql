591a3e9f417c04bafde1ae53376554bd3ca2829e
Yusuke Shimizu <yshimizu@karahiyo.dev>
2022-08-10 22:46:23 +0900

    fix init visit_history2

--- webapp/sql/init.sql
+++ webapp/sql/init.sql
@@ -3,16 +3,7 @@ DELETE FROM visit_history WHERE created_at >= '1654041600';
 UPDATE id_generator SET id=2678400000 WHERE stub='a';
 ALTER TABLE id_generator AUTO_INCREMENT=2678400000;
 
-CREATE TABLE `visit_history2`
-(
-    `tenant_id` BIGINT UNSIGNED NOT NULL,
-    `competition_id` VARCHAR(255) NOT NULL,
-    `player_id` VARCHAR(255) NOT NULL,
-    `created_at` BIGINT NOT NULL,
-    `updated_at` BIGINT NOT NULL,
-    PRIMARY KEY (`tenant_id`, `competition_id`, player_id)
-) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
-
+DELETE FROM visit_history2;
 INSERT INTO visit_history2
 SELECT tenant_id, competition_id, player_id, MIN(created_at) AS created_at, MAX(updated_at) AS updated_at
 FROM visit_history
